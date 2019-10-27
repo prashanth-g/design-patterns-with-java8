88fc20a07867a72a43964409241d204b96c1bde3
prashanth-g <prashanthcsgce@gmail.com>
2019-10-27 19:40:27 +0530

    updated consumer chaining

--- src/main/java/com/prashanth/os/design/patterns/consumer/chaining/function/Consumer.java
+++ src/main/java/com/prashanth/os/design/patterns/consumer/chaining/function/Consumer.java
@@ -1,11 +1,14 @@
 package com.prashanth.os.design.patterns.consumer.chaining.function;
 
+import java.util.Objects;
+
 @FunctionalInterface
 public interface Consumer<T> {
     void accept(T t);
 
     // Concrete method
     default Consumer<T> andThen(Consumer<T> other) {
+        Objects.requireNonNull(other);
         return (T t) -> {
             this.accept(t);
             other.accept(t);
