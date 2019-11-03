408b9f099165e60c5c88aaedd0fc707038826ff2
prashanth-g <prashanthcsgce@gmail.com>
2019-11-03 17:27:02 +0530

    Added concrete reversed method and generic comparing

--- src/main/java/com/prashanth/os/design/patterns/comparators/Comparator.java
+++ src/main/java/com/prashanth/os/design/patterns/comparators/Comparator.java
@@ -8,12 +8,16 @@ public interface Comparator<T> {
 
     int compare(T t1, T t2);
 
-    static <T> Comparator<T> comparing(Function<T, String> keyExtractor) {
+    static <T, U extends Comparable<U>> Comparator<T> comparing(Function<T, U> keyExtractor) {
         Objects.requireNonNull(keyExtractor);
         return (p1, p2) -> {
-            String s1 = keyExtractor.apply(p1);
-            String s2 = keyExtractor.apply(p2);
+            U s1 = keyExtractor.apply(p1);
+            U s2 = keyExtractor.apply(p2);
             return s1.compareTo(s2);
         };
+    }
+
+    default Comparator<T> reversed() {
+        return (t1, t2) -> this.compare(t2, t1);
     }
 }
--- src/main/java/com/prashanth/os/design/patterns/comparators/PlayingWithComparators.java
+++ src/main/java/com/prashanth/os/design/patterns/comparators/PlayingWithComparators.java
@@ -9,14 +9,20 @@ public class PlayingWithComparators {
         Person tina = Person.of("Tina", 7);
 
         Function<Person, String> getName = person -> person.getName();
+        Function<Person, Integer> getAge = person -> person.getAge();
 
         Comparator<Person> comparator = (person1, person2) ->
                 getName.apply(person1).compareTo(getName.apply(person2));
 
         Comparator<Person> compareName = Comparator.comparing(getName);
+        Comparator<Person> compareAge = Comparator.comparing(getAge);
+
+        Comparator<Person> compareNameReversed = compareName.reversed();
 
         System.out.println("Mougli vs Beem: " + (comparator.compare(mougli, beem) > 0));
         System.out.println("Beem vs Tina: " + (comparator.compare(beem, tina) > 0));
+        System.out.println("Mougli vs Beem: " + (compareNameReversed.compare(mougli, beem) > 0));
+        System.out.println("Mougli vs Beem: " + (compareAge.compare(mougli, beem) > 0));
     }
 
 
